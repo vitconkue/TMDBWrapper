ff80babcdd3bb69b38829c804ac36513d82ba2b8
vitconkue <voan0935995700@gmail.com>
2021-07-17 15:56:24 +0700

    update full  movie model

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,19 +1,24 @@
+using System.Collections.Generic;
+
 namespace MainLibrary.Models
 {
     public class Movie
     {
-        public  bool adult { get; set; }
-        public string title  {get; set; }
-
-        public double vote_average {get; set; }
-
-        public int vote_count {get; set; }
-        
+        public bool adult { get; set; }
+        public string backdrop_path { get; set; }
+        public List<int> genre_ids { get; set; }
+        public int id { get; set; }
+        public string media_type { get; set; }
+        public string title { get; set; }
+        public string original_language { get; set; }
+        public string original_title { get; set; }
+        public string overview { get; set; }
         public double popularity { get; set; }
-        
-        public string idmb_id { get; set; }
-        
         public string poster_path { get; set; }
+        public string release_date { get; set; }
+        public bool video { get; set; }
+        public double vote_average { get; set; }
+        public int vote_count { get; set; }
 
         public override string ToString()
         {
